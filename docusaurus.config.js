b95ec3aff05b2ae0b775f2f323aa5a91d1b3e7ed
Mohab <whitearsenic@outlook.com>
2023-01-15 01:03:15 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Intuita Docs',
   tagline: 'Upgrade dependencies faster with high-quality codemods.',
   url: 'https://intutia.io',
-  baseUrl: "/docs/",
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: 'img/favicon.ico',
