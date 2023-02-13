9d0ef23732e1c874e32ae43fab1f891bc253a149
Mohab <whitearsenic@outlook.com>
2023-02-13 13:29:43 +0200

    Update docusaurus.config.js
    closes INT-375

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,6 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          
         },
         theme: {
           customCss: require.resolve('./src/css/custom-dev.css'),
